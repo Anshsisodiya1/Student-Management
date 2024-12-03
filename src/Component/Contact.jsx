d20e2530a80d916b2a0ad915500b3209f940ade7
Ansh Sisodiya <sisodiyaansh785@gmail.com>
2024-12-03 08:09:25 -0700

    Added contact form

--- src/Component/Contact.jsx
+++ src/Component/Contact.jsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React from "react";
+import "./Contact.css";
 
-const Contact = () => {
-  return <h1>Welcome to the Contact Page</h1>;
+const ContactForm = () => {
+  return (
+    <div className="center-wrapper">
+      <h1>Contact Us</h1>
+      <div className="form-cont">
+        <form action="https://getform.io/f/ajjjyqja" method="POST">
+          <input type="text" name="name" placeholder="Your Name" required />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea name="message" placeholder="Your Message" required></textarea>
+          <button type="submit">Send</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
-export default Contact;
+export default ContactForm;
